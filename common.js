4617e2e5295cd41738de24655bdf71c2618d6940
padcmoi <juliennaskot@gmail.com>
2025-03-03 17:28:00 +0100

    git init

---
+++ common.js
@@ -0,0 +1,294 @@
+class SkillCreator {
+  constructor(name, level = "", stars = "", dots = false, { uppercase = false, italic = false } = {}, children = []) {
+    this.name = name;
+    this.level = level;
+    this.stars = stars;
+    this.dots = dots;
+    this.uppercase = uppercase;
+    this.italic = italic;
+    this.children = children;
+  }
+
+  createSkillItem() {
+    const li = document.createElement("li");
+    li.className = "skill-item";
+
+    const nameSpan = document.createElement("span");
+    nameSpan.className = "name";
+    nameSpan.textContent = this.name;
+    if (this.uppercase) nameSpan.style.textTransform = "uppercase";
+    if (this.italic) nameSpan.style.fontStyle = "italic";
+
+    li.appendChild(nameSpan);
+
+    if (this.dots) {
+      const dotsSpan = document.createElement("span");
+      dotsSpan.className = "dots";
+      li.appendChild(dotsSpan);
+    }
+
+    if (this.level) {
+      const levelSpan = document.createElement("span");
+      levelSpan.className = "level";
+      levelSpan.textContent = this.level;
+      levelSpan.classList.add("no-select");
+      li.appendChild(levelSpan);
+    }
+
+    if (this.stars) {
+      const starsDiv = document.createElement("div");
+      starsDiv.className = "stars";
+      starsDiv.textContent = this.stars;
+      starsDiv.classList.add("no-select");
+      li.appendChild(starsDiv);
+    }
+
+    return li;
+  }
+
+  static createSkillsList(skills, parentUl) {
+    skills.forEach((skill) => {
+      const skillItem = skill.createSkillItem();
+      parentUl.appendChild(skillItem);
+
+      if (skill.children.length > 0) {
+        const childUl = document.createElement("ul");
+        childUl.className = "skills-list child";
+        SkillCreator.createSkillsList(skill.children, childUl);
+        parentUl.appendChild(childUl);
+      }
+    });
+  }
+
+  static initialize(container, skillsData) {
+    const createSkill = (skill) => {
+      const options = { uppercase: skill.uppercase, italic: skill.italic };
+      return new SkillCreator(skill.name, skill.level, skill.stars, skill.dots, { ...options }, (skill.children || []).map(createSkill));
+    };
+
+    const skills = skillsData.map(createSkill);
+
+    const skillsList = document.createElement("ul");
+    skillsList.className = "skills-list margin-right";
+    container.appendChild(skillsList);
+    SkillCreator.createSkillsList(skills, skillsList);
+  }
+}
+
+class TimelineEvents {
+  constructor(targetElement, events = []) {
+    this.targetElement = targetElement;
+    this.events = events;
+
+    this.render();
+  }
+
+  renderDescription(description) {
+    const renderList = (element, children) => {
+      return `<${element}>${children
+        .map((child) => {
+          if (typeof child === "string") {
+            return `<li>${child}</li>`;
+          } else {
+            return `<li>${renderList(child.element, child.children)}</li>`;
+          }
+        })
+        .join("")}</${element}>`;
+    };
+
+    return description
+      .map((desc) => {
+        if (desc.element === "custom") {
+          return `${desc.content}`;
+        } else if (desc.element === "p") {
+          return `<p>${desc.content}</p>`;
+        } else if (desc.element === "ul" || desc.element === "ol") {
+          return renderList(desc.element, desc.children);
+        }
+        return "";
+      })
+      .join("");
+  }
+
+  renderEvent(event) {
+    const locationLink = event.localize ? `<a href="#" onclick="GoogleMapModal.initialize('${event.localize.city}', '${event.localize.country}').openModal()">${event.location}</a>` : event.location;
+    const companyContent = event.companyUrl ? `<a href="${event.companyUrl}" target="_blank">${event.company}</a>` : event.company;
+
+    return `
+      <article class="timeline-event">
+        <div class="timeline-event-content three-rows" data-start="${event.startDate}" data-end="${event.endDate}" data-separator="-">
+          ${event.title ? `<h3>${event.title}</h3>` : ""}
+
+          ${
+            event.legalStatus || event.company || event.location
+              ? `<div ${event.descriptionClasses && event.descriptionClasses.length > 0 ? `class="${event.descriptionClasses.join(" ")}"` : ""}>
+                  ${event.legalStatus ? `[<abbr>${event.legalStatus}</abbr>]` : ""}
+                  ${event.company ? `<span>${companyContent}</span>` : ""}
+                  ${event.company && event.location ? `<small>-</small>` : ""}
+                  ${event.location ? `<address>${locationLink}</address>` : ""}
+                </div>`
+              : ""
+          }
+          ${this.renderDescription(event.description)}
+        </div>
+      </article>
+    `;
+  }
+
+  render() {
+    this.events.forEach((event) => (this.targetElement.innerHTML += this.renderEvent(event)));
+  }
+}
+
+class GoogleMapModal {
+  constructor(city, country, location) {
+    this.city = city;
+    this.country = country;
+    this.location = location;
+    this.mapUrl = `https://www.google.com/maps?q=${encodeURIComponent(this.city + ", " + this.country)}&output=embed&t=k`;
+    this.modal = null;
+    this.createModal();
+  }
+
+  createModal() {
+    this.modal = document.createElement("div");
+    this.modal.id = "myModal";
+    this.modal.style.display = "none";
+    this.modal.style.position = "fixed";
+    this.modal.style.zIndex = "9999";
+    this.modal.style.left = "0";
+    this.modal.style.top = "0";
+    this.modal.style.width = "100%";
+    this.modal.style.height = "100vh";
+    this.modal.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+    this.modal.style.overflow = "auto";
+
+    const modalContent = document.createElement("div");
+    modalContent.style.backgroundColor = "#fefefe";
+    modalContent.style.margin = "auto";
+    modalContent.style.padding = "0.25em";
+    modalContent.style.border = "1px solid #888";
+    modalContent.style.width = "90%";
+    modalContent.style.maxWidth = "1200px";
+    modalContent.style.maxHeight = "90vh";
+    modalContent.style.overflowY = "auto";
+    modalContent.style.position = "relative";
+
+    const closeContainer = document.createElement("div");
+    closeContainer.style.textAlign = "right";
+    closeContainer.style.marginRight = "0.5em";
+
+    const closeButton = document.createElement("span");
+    closeButton.innerHTML = "&times;";
+    closeButton.style.color = "#333";
+    closeButton.style.fontSize = "2em";
+    closeButton.style.fontWeight = "bold";
+    closeButton.style.cursor = "pointer";
+
+    closeButton.onmouseover = () => {
+      closeButton.style.color = "black";
+      closeButton.style.textDecoration = "none";
+    };
+
+    closeButton.onmouseout = () => {
+      closeButton.style.color = "#333";
+    };
+
+    closeContainer.appendChild(closeButton);
+
+    const iframeContainer = document.createElement("div");
+    iframeContainer.style.width = "100%";
+    iframeContainer.style.height = "0";
+    iframeContainer.style.paddingBottom = "56.25%"; /* 16:9 aspect ratio */
+    iframeContainer.style.position = "relative";
+
+    const iframe = document.createElement("iframe");
+    iframe.id = "mapFrame";
+    iframe.frameBorder = "0";
+    iframe.style.border = "0";
+    iframe.allowFullscreen = true;
+    iframe.style.position = "absolute";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+
+    iframeContainer.appendChild(iframe);
+    modalContent.appendChild(closeContainer);
+    modalContent.appendChild(iframeContainer);
+    this.modal.appendChild(modalContent);
+    document.body.appendChild(this.modal);
+
+    closeButton.onclick = () => this.closeModal();
+    window.onclick = (event) => {
+      if (event.target == this.modal) {
+        this.closeModal();
+      }
+    };
+  }
+
+  openModal() {
+    this.modal.style.display = "block";
+    this.modal.querySelector("#mapFrame").src = this.mapUrl;
+  }
+
+  closeModal() {
+    this.modal.style.display = "none";
+  }
+
+  static initialize(city, country, location) {
+    return new GoogleMapModal(city, country, location);
+  }
+}
+
+function printOrDownloadPDF() {
+  const elements = document.querySelectorAll(".typing-effect, .typing-effect-delayed");
+  elements.forEach((element) => {
+    element.classList.forEach((className) => {
+      if (className === "typing-effect" || className === "typing-effect-delayed") {
+        element.classList.replace(className, `___${className}`);
+      }
+    });
+  });
+
+  const restoreTitle = document.title;
+  document.title = "CV_Julien_Jean_developer_fullstack_JavaScript";
+
+  window.print();
+
+  elements.forEach((element) => {
+    element.classList.forEach((className) => {
+      if (className === "___typing-effect" || className === "___typing-effect-delayed") {
+        element.classList.replace(className, className.substring(3));
+      }
+    });
+  });
+
+  document.title = restoreTitle;
+}
+
+function startTypingEffects() {
+  function processTypingEffects() {
+    const typingElements = document.querySelectorAll(".typing-effect");
+
+    if (typingElements.length > 0) {
+      typingElements.forEach((el) => el.classList.remove("typing-effect"));
+    }
+
+    const delayedElements = document.querySelectorAll(".typing-effect-delayed");
+    if (delayedElements.length > 0) {
+      delayedElements[0].classList.replace("typing-effect-delayed", "typing-effect");
+      setTimeout(processTypingEffects, 3500);
+    }
+  }
+
+  if ((document.querySelectorAll(".typing-effect")?.length ?? 0) === 0) {
+    processTypingEffects();
+  } else {
+    setTimeout(() => processTypingEffects(), 3500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  startTypingEffects();
+});
---
+++ data.js
@@ -0,0 +1,182 @@
+// skills
+const skillsData = [
+  { name: "VanillaJS", level: "6 ans", stars: "★★★★★", dots: true },
+  { name: "NodeJS", stars: "★★★★☆", dots: true },
+  { name: "TypeScript", level: "2 ans", stars: "★★★☆☆", dots: true },
+  {
+    name: "Ecosystème Vue.js",
+    uppercase: true,
+    children: [
+      { name: "Vue v2 & 3", level: "5 ans", stars: "★★★★★", dots: true },
+      { name: "Nuxt v3", level: "1 an", stars: "★★★☆☆", dots: true },
+      {
+        name: "Bibliothèques de composants:",
+        children: [
+          { name: "VueUse", stars: "★★★★★", dots: true },
+          { name: "BootstrapVue", stars: "★★★★★", dots: true },
+          { name: "Vuetify", stars: "★★★★★", dots: true },
+          { name: "Quasar", stars: "★★★☆☆", dots: true },
+          { name: "NaiveUI", stars: "★★★☆☆", dots: true },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Ecosystème React.js",
+    uppercase: true,
+    children: [
+      { name: "React v19", level: "débutant", stars: "★☆☆☆☆", dots: true },
+      { name: "Next v15", level: "débutant", stars: "★☆☆☆☆", dots: true },
+      {
+        name: "Bibliothèques de composants:",
+        children: [{ name: "Mui", stars: "☆☆☆☆☆", dots: true }],
+      },
+    ],
+  },
+  {
+    name: "CSS",
+    uppercase: true,
+    children: [
+      { name: "Css3", stars: "★★★★☆", dots: true },
+      { name: "Bootstrap v5", stars: "★★★★☆", dots: true },
+      { name: "Tailwind v4", stars: "★★☆☆☆", dots: true },
+    ],
+  },
+  {
+    name: "ORM / ODM / QB",
+    uppercase: true,
+    children: [
+      { name: "Sequelize", stars: "★★★★☆", dots: true },
+      { name: "TypeORM", stars: "★★★☆☆", dots: true },
+      { name: "Prisma", stars: "★★★☆☆", dots: true },
+      { name: "Mongoose (mongoDB)", stars: "★★★☆☆", dots: true },
+    ],
+  },
+];
+SkillCreator.initialize(document.getElementById("skills-id"), skillsData);
+
+// timeline professionnal career
+new TimelineEvents(document.getElementById("professional-experiences"), [
+  {
+    startDate: "11 avril 2022",
+    endDate: "29 février 2024",
+    title: "Développeur frontend JavaScript (Contrat de professionnalisation)",
+    legalStatus: "SAS",
+    company: "ExAssess (VTEST)",
+    companyUrl: "https://www.vtest.com/",
+    location: "Paris (100% télétravail)",
+    localize: { city: "Paris", country: "FRANCE" },
+    description: [
+      {
+        element: "p",
+        content: `Développement d'une application en VanillaJS permettant d’évaluer les compétences en anglais d’un candidat sur quatre aspects : oral, écoute, écriture et lecture.`,
+      },
+      { element: "p", content: `Fonctionnalités de l'application :` },
+      {
+        element: "ul",
+        children: [
+          `Capture en direct du micro de l'utilisateur.`,
+          `Mise en cache des sons écoutés pour garantir leur disponibilité.`,
+          `Déplacement des éléments en Drag and Drop.`,
+          `Utilisation d'un timer pour guider l'utilisateur à travers différents exercices et évaluer la rapidité avant de sauvegarder les résultats vers l'API.`,
+        ],
+      },
+      { element: "p", content: `Étroitement collaboré avec l’équipe backend sur les endpoints, avec des contributions sur l’API NestJS.` },
+      { element: "custom", content: `<div class="my-1"></div>` },
+      {
+        element: "p",
+        content: `Gestion de projet sur ClickUp, réunions quotidiennes et déploiement sur AWS (DevOps). Création de composants sur une application Nuxt 3 avec TypeScript et correctifs.`,
+      },
+    ],
+  },
+  {
+    startDate: "octobre 2021",
+    endDate: "décembre 2021",
+    title: "Développeur web fullstack JavaScript (Freelance)",
+    company: "Skyview innovation",
+    companyUrl: "https://www.skyviewinnovation.fr/",
+    location: "Menton",
+    localize: { city: "Menton", country: "FRANCE" },
+    description: [
+      {
+        element: "p",
+        content: `Création d'un Dashboard avec Vue.js et une API Express pour un service de rencontre en ligne, incluant la gestion des utilisateurs et des ACLs pour les autorisations (admin/modérateur).`,
+      },
+    ],
+  },
+  {
+    startDate: "mai 2020",
+    endDate: "juillet 2020",
+    title: "Développeur web fullstack (Stage pour le titre professionnel)",
+    company: "Les petits sabots 64",
+    companyUrl: "https://www.societe.com/societe/monsieur-didier-supervielle-792272056.html",
+    location: "Monein (100% télétravail)",
+    localize: { city: "Monein", country: "FRANCE" },
+    description: [
+      {
+        element: "p",
+        content: `
+        Lors de ce stage, j'ai collaboré avec un centre équestre qui rencontrait des difficultés avec son site web. 
+Leur agence web facturait chaque ajout d'images, ce qui posait problème pour le centre équestre.
+        `,
+      },
+      { element: "custom", content: `<div class="my-1"></div>` },
+      {
+        element: "p",
+        content: `J'ai donc développé un site web complet avec Nuxt.js v2(optimisé pour la SEO) et une API Symfony v5, permettant au client de gérer facilement les images et les informations des chevaux. Les fonctionnalités incluaient :`,
+      },
+      {
+        element: "ul",
+        children: [`Gestion des images et conversion au format webp (plus leger).`, `Gestion des chevaux (ajout, modification, suppression).`, `Rappels pour les soins des chevaux (vermifuge, ferrage, etc.).`],
+      },
+      { element: "p", content: "Malgré des mesures de sécurité rigoureuses (API), le client a souhaité retirer l'application par crainte de fuite de données." },
+    ],
+  },
+]);
+
+// certifications
+new TimelineEvents(document.getElementById("passed-certifications"), [
+  {
+    startDate: "septembre 2024",
+    endDate: "février 2025",
+    company: "DYMA",
+    companyUrl: "https://dyma.fr/",
+    location: "Formation e-learning",
+    descriptionClasses: ["custom-div-flex1"],
+    description: [
+      {
+        element: "p",
+        content: `Certifiées: ${["Linux & BASH", "GIT", "Docker", "Typescript", "React 19", "Next 15"].join(", ")}`,
+      },
+    ],
+  },
+  {
+    startDate: "11 avril 2022",
+    endDate: "3 février 2024",
+    title: "Bachelor Concepteur développeur d'applications",
+    company: "O'clock",
+    companyUrl: "https://oclock.io/",
+    location: "En ligne",
+    description: [
+      {
+        element: "p",
+        content: `Diagramme UML, programmation orientée objet, déploiement intégration continue, docker, tests unitaires, typescript, API`,
+      },
+    ],
+  },
+  {
+    startDate: "1 décembre 2019",
+    endDate: "30 aout 2020",
+    title: "Titre professionnel développeur web & web mobile",
+    company: "DYMA",
+    companyUrl: "https://dyma.fr/",
+    location: "Nice",
+    localize: { city: "Nice", country: "FRANCE" },
+    description: [
+      {
+        element: "p",
+        content: `Symfony 5, PHP, JS, base de données SQL et conception`,
+      },
+    ],
+  },
+]);
